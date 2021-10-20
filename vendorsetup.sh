736bc8ed503db811e8e676b25cbe89ca0595294a
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-10-20 04:09:45 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,12 +22,13 @@ git clone https://gitlab.com/Ardjlon/firmware_xiaomi_surya firmware/xiaomi/surya
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 prebuilts/gcc/linux-x86/aarch64/aarch64-elf
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm prebuilts/gcc/linux-x86/arm/arm-eabi
 #Fix Kernel storm
-git -C kernel/xiaomi/surya revert e28dfd2aaba38e0883b6fcf37231ee14e244c54d -n -e
-git -C kernel/xiaomi/surya revert 4988721bb60be00e7046de1264334acc3cf9dd17 -n -e 
-git -C kernel/xiaomi/surya revert 3a7a861f01bf47dc6162b810dad48b8895ec85e0 -n -e
-git -C kernel/xiaomi/surya revert 8e8475087a44da20c1078afbd8fc9878e373a9dc -n -e 
-git -C kernel/xiaomi/surya revert 81b34dcd608eaef73530d73406ace8997226e2c6 -n -e 
-git -C kernel/xiaomi/surya revert 373a85e58fcf3966a5751ce3f6bf1215e013c6de -n -e 
-git -C kernel/xiaomi/surya revert 7614a711f798a7517f1ff60546fcbb700873f7f6 -n -e
+cd kernel/xiaomi/surya
+git revert e28dfd2aaba38e0883b6fcf37231ee14e244c54d -n -e
+git revert 4988721bb60be00e7046de1264334acc3cf9dd17 -n -e 
+git revert 3a7a861f01bf47dc6162b810dad48b8895ec85e0 -n -e
+git revert 8e8475087a44da20c1078afbd8fc9878e373a9dc -n -e 
+git revert 81b34dcd608eaef73530d73406ace8997226e2c6 -n -e 
+git revert 373a85e58fcf3966a5751ce3f6bf1215e013c6de -n -e 
+git revert 7614a711f798a7517f1ff60546fcbb700873f7f6 -n -e
 #fix changue kernel
-git -C kernel/xiaomi/surya revert 928212482106338cfd5ad43e9f02d648ba0d0df1 -n -e
+git revert 928212482106338cfd5ad43e9f02d648ba0d0df1 -n -e
