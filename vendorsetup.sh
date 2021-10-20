a68e0901216737749694595de448642c6e2c299a
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-10-20 03:09:48 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,10 +8,6 @@ git clone https://github.com/FabricioSC123/vendor_PocoParts.git vendor/PocoParts
 git clone https://github.com/FabricioSC123/android_vendor_support.git vendor/support
 # fix error pixel kernel
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
-# Proton
-git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-13 --depth=1
-touch prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
-echo "13" >> prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
 # Disable Realme partitions
 git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
 git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
