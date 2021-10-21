7585da3b6fd25898f8cedc6b86349ef3b20a08d4
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-10-21 02:32:46 -0500

    Update and rename cherish_surya.mk to descendant_surya.mk

--- descendant_surya.mk
+++ descendant_surya.mk
@@ -21,9 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/descendant/config/common.mk)
 
-PRODUCT_NAME := cherish_surya
+PRODUCT_NAME := descendant_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
